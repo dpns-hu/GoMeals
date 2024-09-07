607246550df2e5e033e15fa6acb2f9eb63f984c6
Deepanshu <deepanshukushwaha7389@gmail.com>
2024-09-07 16:34:47 +0530

    Added Login Via Viewmodel

--- app/src/main/java/com/example/gomeals/data/repository/AuthRepositoryImpl.kt
+++ app/src/main/java/com/example/gomeals/data/repository/AuthRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.example.gomeals.data.repository
 
+import android.util.Log
 import com.example.gomeals.domain.repository.AuthRepository
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.coroutines.flow.Flow
@@ -10,14 +11,14 @@ import kotlinx.coroutines.tasks.await
 class AuthRepositoryImpl(private val auth: FirebaseAuth) :AuthRepository {
 
     override fun login(email: String, password: String): Flow<Boolean> = flow {
-        try {
-            auth.signInWithEmailAndPassword(email, password).await()
-            emit(true) // Login successful
-        } catch (exception: Exception) {
-            emit(false) // Login failed
-        }
+        // This will emit true only if the login is successful
+        auth.signInWithEmailAndPassword(email, password).await()
+        Log.d("AuthRepository","Login successful")
+        emit(true) // Login successful
     }.catch { exception ->
-        emit(false) // Handle exceptions and emit failure
+        // Handle errors here
+        Log.d("AuthRepository","Login Failed: ${exception.message}")
+        emit(false) // Login failed
     }
 
 }
--- app/src/main/java/com/example/gomeals/presentation/activity/Login_page.kt
+++ app/src/main/java/com/example/gomeals/presentation/activity/Login_page.kt
@@ -2,14 +2,13 @@ package com.example.gomeals.presentation.activity
 
 import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
-import androidx.lifecycle.LifecycleOwner
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
-import androidx.lifecycle.lifecycleScope
 import com.example.gomeals.R
 import com.example.gomeals.databinding.ActivityLoginPageBinding
 import com.example.gomeals.presentation.viewmodel.AuthViewModel
@@ -21,96 +20,103 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.database.FirebaseDatabase
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class Login_page : AppCompatActivity() {
-    lateinit var binding: ActivityLoginPageBinding
-      lateinit var name :String
-     lateinit var email :String
-     lateinit var password :String
-     lateinit var auth : FirebaseAuth
-     lateinit var database : FirebaseDatabase
-     lateinit var googleSignInClient :GoogleSignInClient
-       val authViewModel: AuthViewModel by viewModels()
+
+    private lateinit var binding: ActivityLoginPageBinding
+    private lateinit var auth: FirebaseAuth
+    private lateinit var database: FirebaseDatabase
+    private lateinit var googleSignInClient: GoogleSignInClient
+
+    private val authViewModel: AuthViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginPageBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         auth = FirebaseAuth.getInstance()
         database = FirebaseDatabase.getInstance()
 
-          val signInOption = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken(getString(
-              R.string.default_web_client_id
-          )).requestEmail().build()
-            googleSignInClient = GoogleSignIn.getClient(this,signInOption)
+        val signInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, signInOptions)
+
         binding.googleSignUpPage.setOnClickListener {
             val signInClient = googleSignInClient.signInIntent
             launcher.launch(signInClient)
         }
-     binding.sentsignupLoginpage.setOnClickListener {
-         val intent = Intent(this, Signup_page::class.java)
-         startActivity(intent)
-     }
-        binding.loginButton.setOnClickListener{
-            email = binding.emailLogin.text.toString().trim()
-            password = binding.passwordLoginn.text.toString().trim()
-            if(email.isBlank() || password.isBlank()){
-                Toast.makeText(this,"Fill all the details",Toast.LENGTH_SHORT).show()
-            }
-            else{
-                signInUser(email,password)
+
+        binding.sentsignupLoginpage.setOnClickListener {
+            val intent = Intent(this, Signup_page::class.java)
+            startActivity(intent)
+        }
+
+        binding.loginButton.setOnClickListener {
+            val email = binding.emailLogin.text.toString().trim()
+            val password = binding.passwordLoginn.text.toString().trim()
+            if (email.isBlank() || password.isBlank()) {
+                Toast.makeText(this, "Fill all the details", Toast.LENGTH_SHORT).show()
+            } else {
+                signInUser(email, password)
             }
         }
+
+        observeViewModel()
     }
 
     override fun onStart() {
         super.onStart()
-         val currentUser = auth.currentUser
-        if(currentUser!=null){
+        auth.currentUser?.let {
             updateUI()
         }
-
     }
 
-    private fun signInUser(email: String, password: String) {
-        authViewModel.login(email,password)
+    private fun observeViewModel() {
         authViewModel.loginResponse.observe(this, Observer { isLoggedIn ->
             if (isLoggedIn) {
-                 UiUtil.showToast(this,"Login Successful")
+                UiUtil.showToast(this, "Login Successful")
+                updateUI()
             } else {
-                 UiUtil.showToast(this,"Login Failed")
+                UiUtil.showToast(this, "Login Failed")
             }
         })
     }
 
+    private fun signInUser(email: String, password: String) {
+        authViewModel.login(email, password)
+    }
+
     private fun updateUI() {
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
+        startActivity(Intent(this, MainActivity::class.java))
         finish()
     }
-    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
-        result->
-            if(result.resultCode==Activity.RESULT_OK){
-                      val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
-                if(task.isSuccessful){
-                    val account:GoogleSignInAccount?=task.result
-                    val credential = GoogleAuthProvider.getCredential(account?.idToken,null)
-                    auth.signInWithCredential(credential).addOnCompleteListener {
-                        if(it.isSuccessful){
-                            Toast.makeText(this,"Login Successful",Toast.LENGTH_SHORT).show()
+
+    private val launcher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                if (task.isSuccessful) {
+                    val account: GoogleSignInAccount? = task.result
+                    val credential = GoogleAuthProvider.getCredential(account?.idToken, null)
+                    auth.signInWithCredential(credential).addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            UiUtil.showToast(this, "Login Successful")
                             startActivity(Intent(this, MainActivity::class.java))
                             finish()
-
-                        }else{
-                            Toast.makeText(this,"Login Failed",Toast.LENGTH_SHORT).show()
+                        } else {
+                            UiUtil.showToast(this, "Login Failed")
                         }
                     }
-
+                } else {
+                    UiUtil.showToast(this, "Login Failed")
                 }
-            }else{
-                Toast.makeText(this,"Login Failed",Toast.LENGTH_SHORT).show()
+            } else {
+                UiUtil.showToast(this, "Login Failed")
             }
-    }
-
+        }
 }
